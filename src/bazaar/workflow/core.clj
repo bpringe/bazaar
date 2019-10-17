bea3535bb019d2afac30621c140dd46dad3422ab
Brandon Ringe <brandon.ringe@gmail.com>
2019-10-16 20:11:08 -0700

    Add example data structure for processes

--- src/bazaar/workflow/core.clj
+++ src/bazaar/workflow/core.clj
@@ -1,6 +1,25 @@
 (ns bazaar.workflow.core
   (:require [bazaar.processes.core-async :as pca]))
 
+;;;; Example data structure for processes
+
+(def workflow-1
+  [[:p1 :p2]])
+
+(def workflow-2
+  [[:workflow-1 :p3]])
+
+(def workflow-3
+  [[:workflow-2 :p4]])
+
+;; For workflow-3
+(def processes-map {[:workflow-3 :workflow-2 :workflow-1 :p1] {#_p1_map}
+                    [:workflow-3 :workflow-2 :workflow-1 :p2] {#_p2_map}
+                    [:workflow-3 :workflow-2 :p3] {#_p3_map}
+                    [:workflow-3 :p4] {#_p4_map}})
+
+;;;; Helper functions
+
 (defn get-base-process
   [process-fn]
   (let [metadata (meta process-fn)]
